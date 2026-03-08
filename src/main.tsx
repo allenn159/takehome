19a5d82b09ed9a877fee1225676b2b42fbc00e5f
Allen Abbott <allenabbottdev@gmail.com>
2026-03-08 14:25:30 -0700

    set staleTime to prevent refetching static data

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,13 @@ import { Provider as ChakraProvider } from "@/components/ui/provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/context/ThemeContext";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
